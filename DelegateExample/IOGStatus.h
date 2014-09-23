57847cd9cce640a66a8e42f873d68847416f95d0
Joan Teixidó <joan@laiogurtera.com>
2014-09-24 00:24:39 +0200

    init

---
+++ DelegateExample/IOGStatus.h
@@ -0,0 +1,13 @@
+//
+//  IOGStatus.h
+//  DelegateExample
+//
+//  Created by Joan Teixidó on 24/09/14.
+//  Copyright (c) 2014 LaIogurtera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IOGStatus : NSObject
+
+@end
---
+++ DelegateExample/IOGStatus.m
@@ -0,0 +1,13 @@
+//
+//  IOGStatus.m
+//  DelegateExample
+//
+//  Created by Joan Teixidó on 24/09/14.
+//  Copyright (c) 2014 LaIogurtera. All rights reserved.
+//
+
+#import "IOGStatus.h"
+
+@implementation IOGStatus
+
+@end
